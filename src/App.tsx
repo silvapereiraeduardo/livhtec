e758d9da47467835e48085470fc919fb337de2d0
eduardo.pereira <silvapereiraeduardo@gmail.com>
2023-06-01 11:51:29 -0300

    primeira versao do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex h-100 text-center text-bg-dark">
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header className="mb-auto"></header>
+
+        <main className="px-3">
+          <h1>livhtec</h1>
+          <p className='lead fst-italic'>Coming Soon...</p>
+        </main>
+
+        <footer className="mt-auto text-white-50">
+          {
+            /**
+             * <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
+             */
+          }
+        </footer>
+      </div>
     </div>
   );
 }
