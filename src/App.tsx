7cc18ef7b4d6a1a257de62efe6de9d2884df2db1
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2023-09-12 18:16:28 -0300

    sesao de servicos

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import { TestimonialsSection } from "./components/TestimonialsSection";
 import { Footer } from "./components/Footer";
 import { CenterAlignedCallToAction } from "./components/CenterAlignedCallToAction";
 import { LeftAlignedCallToAction } from "./components/LeftAlignedCallToAction";
+import { ServiceSection } from "./components/ServicesSection";
 
 function App() {
   return (
     <>
       <Header />
       <HeroSection />
+      <ServiceSection />
       <FeaturiesSection />
       <LeftAlignedCallToAction />
       <TestimonialsSection />
